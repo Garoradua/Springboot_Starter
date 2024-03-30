d0276fb7b5a7636ae91fbaa4956d763d1badd023
Gourav Arora <gouravarora035@gmail.com>
2024-03-30 22:37:58 +0530

    starting

--- src/main/java/com/example/firstspringbootApplication/FirstApp/rest/FirstAppController.java
+++ src/main/java/com/example/firstspringbootApplication/FirstApp/rest/FirstAppController.java
@@ -1,5 +1,6 @@
 package com.example.firstspringbootApplication.FirstApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,25 @@ public class FirstAppController {
     public String sayHello(){
         return "Hello World";
     }
-    @GetMapping("/2")
+    @GetMapping("/hello")
     public String sayHello1(){
         return "Hello World1";
     }
+
+    @GetMapping("/fortune")
+    public String sayHello2(){
+        return "Hello World1";
+    }
+
+    @Value("${first.name}")
+    private String firstName;
+
+    @Value("${last.name}")
+    private String lastName;
+
+    @GetMapping("/myinfo")
+    public String  myinfo(){
+        return "firstname " + firstName + ", Lastname " + lastName;
+    }
+
 }
